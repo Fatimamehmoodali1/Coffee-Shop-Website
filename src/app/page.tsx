f49e767999ada2e33c37f25872666c5f5efad2e5
Fatimamehmoodali1 <162306190+Fatimamehmoodali1@users.noreply.github.com>
2024-11-09 12:44:05 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+import Image from "next/image";
+import Hero from "@/components/Hero";
+import About from "@/components/About"
+import Menu from "@/components/Menu"
+import Blogs from "@/components/Blogs"
+import Contact from "@/components/Contact"
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Menu />
+      <Blogs />
+      <Contact />
+    </div>
+  );
+}
